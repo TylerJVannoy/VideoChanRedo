e3c22d865e3be38b0b1fdf7a7ec0979075873741
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-10-25 18:37:31 -0700

    delete comments

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -26,39 +26,3 @@ const SearchBar = ({ search }) => {
 };
 
 export default SearchBar;
-
-// export default SearchBar;
-
-/*        <div class="container">
-          <div class="row height d-flex justify-content-center align-items-center">
-            <div class="col-md-6">
-              <div class="form">
-                {" "}
-                <i class="fa fa-search"></i>{" "}
-                <input
-                  type="text"
-                  class="form-control form-input"
-                  placeholder="Search anything..."
-                />{" "}
-                <span class="left-pan">
-                  <i class="fa fa-microphone"></i>
-                </span>{" "}
-              </div>
-            </div>
-          </div>
-        </div>
-        
-            <div className="search-bar ui segment">
-      <form className="ui form" onSubmit={onSubmit}>
-        <div className="field">
-          <label>Search For a Video</label>
-          <input
-            value={term}
-            type="text"
-            onChange={event => setTerm(event.target.value)}
-          ></input>
-        </div>
-      </form>
-    </div>
-        
-        */
